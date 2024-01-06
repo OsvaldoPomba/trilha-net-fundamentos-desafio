f3b42cc5e5830cc546c757c86164a3d82d0e6306
Guilherme <guizampier@gmail.com>
2024-01-05 21:10:42 -0300

    DesafiosFUndamentos Feito

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,67 +1,164 @@
-namespace DesafioFundamentos.Models
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioProjetoEstacionamento.Estacionamento
 {
-    public class Estacionamento
+    public class Estacionar
     {
-        private decimal precoInicial = 0;
-        private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private  int _taxaEntradaDoEstacionamento;
+        private int _horaEstacionamento;
 
-        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        List<String> placaDoCarro = new List<String>();
+        
+        public int horaEstacionamento
         {
-            this.precoInicial = precoInicial;
-            this.precoPorHora = precoPorHora;
-        }
-
-        public void AdicionarVeiculo()
-        {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
-        }
-
-        public void RemoverVeiculo()
-        {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            get => _horaEstacionamento;
+            set 
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
-            }
-            else
-            {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+               while(value <= 0)
+               {
+                    Console.WriteLine("Não é permitido este valor!\n Informe um valor maior");
+                    value = Convert.ToInt32(Console.ReadLine());
+               }
+               _horaEstacionamento = value;
             }
         }
-
-        public void ListarVeiculos()
+        public int taxaEntradaDoEstacionamento
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            get => _taxaEntradaDoEstacionamento;
+            set 
             {
-                Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
-            }
-            else
-            {
-                Console.WriteLine("Não há veículos estacionados.");
+               while(value <= 0)
+               {
+                    Console.WriteLine("\nNão é permitido este valor!\n Informe um valor maior");
+                    value = Convert.ToInt32(Console.ReadLine());
+               }
+               _taxaEntradaDoEstacionamento = value;
             }
         }
+        public int escolha = 0;
+
+        
+        public void Estacionamento()
+        {
+                Console.WriteLine("Bem vindo ao estacionamento");
+                Console.WriteLine("Digite o valor da entrada do estacionamento:");
+                taxaEntradaDoEstacionamento = Convert.ToInt32(Console.ReadLine());            
+                //Console.WriteLine($"O valor da entrada do estacionamento é de: {taxaEntradaDoEstacionamento:C}\n");
+            
+                Console.WriteLine("Digite o valor gasto por hora no estacionamento:");
+                horaEstacionamento = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine($"O valor gasto por hora no estacionamento é de: {horaEstacionamento:C}\n");
+                int contador = 2;
+                int cadastro = 0;
+
+            while(escolha >= 0)
+            {
+                int i = 0;
+
+                Console.WriteLine("\nO que você deseja fazer? \n 1 - Estacionar veículo\n 2 - Remover veículos\n 3 - Mostrar veículos\n 4 - Encerrar ");
+                escolha = Convert.ToInt32(Console.ReadLine());
+                switch (escolha)
+                {
+                    case 1:
+                        Console.WriteLine("Informe os digitos da placa");
+                        string nomeDaPlaca = Console.ReadLine();
+                        if(placaDoCarro.Contains(nomeDaPlaca))
+                        {
+                            Console.WriteLine("O digíto da placa não pode ser o mesmo");
+                        }
+                        else
+                        {
+                            placaDoCarro.Add(nomeDaPlaca);
+                            if(placaDoCarro.Count >= cadastro)
+                            {
+                                cadastro++;
+                                Console.WriteLine($"{cadastro}º Veículo estacionado com sucesso");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ouve erro ao estacionar o veículo");
+                            }
+                        }
+                        break;
+
+
+                    case 2:
+                        Console.WriteLine("Informe os digitos da placa");
+                        string removerPlaca = Console.ReadLine();
+
+                            if(placaDoCarro.Contains(removerPlaca))
+                            {
+                                Console.WriteLine("Antes de remover o veículo, quantas horas ele ficou estacionado?");
+                                int horasEstacionadas = Convert.ToInt32(Console.ReadLine());
+                                int pagarEstacionamento = taxaEntradaDoEstacionamento + (horaEstacionamento * horasEstacionadas) ;
+                                Console.WriteLine($"Valor a ser pago é de {pagarEstacionamento:C}");
+                                Console.WriteLine($"O Veículo com a plca {removerPlaca} foi removido com sucesso");
+                                placaDoCarro.Remove(removerPlaca);
+
+                            }
+                            else
+                            {
+                                 Console.WriteLine("O nome informado não coincide");
+                            }
+                    break;
+
+
+                    case 3:
+                        if(placaDoCarro.Count > 0)
+                        {
+                            Console.WriteLine("Essa é a placa dos veículos estacionados:");
+                            foreach (string veiculos in placaDoCarro)
+                            {
+                                i++;
+                                Console.WriteLine($"Veículo - {i}º placa = {veiculos}" );
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nenhum veículo foi estacionado");
+                        }
+                    break;
+
+
+                    case 4:
+                    if(placaDoCarro.Count > 0)
+                    {
+                        Console.WriteLine("Que pena que já vai embora \nAntes de ir, me informe quantas horas ficou estacionado por favor");
+                        int horasEstacionadasEmbora = Convert.ToInt32(Console.ReadLine());
+                        int pagarEstacionamentoEmbora = (cadastro * taxaEntradaDoEstacionamento) + (horasEstacionadasEmbora * horaEstacionamento);
+                        Console.WriteLine($"O valor a ser pago é de {pagarEstacionamentoEmbora:C}");
+                        Console.WriteLine("Thau até breve");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("O senhor não tem nada a pagar\nThau até breve");
+                        Environment.Exit(0);
+                    }                       
+                    break;
+
+
+                    default:
+                    while(escolha != 1 || escolha != 2 ||escolha != 3 ||escolha != 4)
+                    {
+                        Console.WriteLine("Digite uma dessas opções: \n 1 - Estacionar veículo\n 2 - Remover veículos\n 3 - Mostrar veículos\n 4 - Encerrar ");
+                        escolha = Convert.ToInt32(Console.ReadLine());
+                        contador+=1;
+                        if (contador > 3)
+                        {
+                            Console.WriteLine("Você teve 3 chances, infelizmente iremos ter que expulsa-lo e muta-lo");
+                            int multa = taxaEntradaDoEstacionamento * horaEstacionamento * 5;
+                            Console.WriteLine($"O valor que deve ser pago é de {multa:C}");
+                            Environment.Exit(0);
+                        }
+                    }
+                    break;
+                }
+            }
+        }      
     }
 }
